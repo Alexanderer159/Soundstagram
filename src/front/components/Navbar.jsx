0d21f52dbb4928fb8046d69662dd7da4e1a52a36
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2025-06-27 17:14:20 +0000

    not enough time

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
+		<nav className="navbar navbar-dark bg-dark">
+			<div className="container d-flex flex-row">
+				<img src="public/Soundstagram_icon.PNG" className="img-fluid w-25 h-25 m-0 p-0"/>
+					<span className="navbar-brand h1">Soundstagram</span>
 				<div className="ml-auto">
 					<Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
