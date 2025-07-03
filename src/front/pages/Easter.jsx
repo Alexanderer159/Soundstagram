63925492637a112ab01a1e80f6237c62228ce07c
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2025-07-03 13:47:55 +0000

    Pending changes exported from your codespace

---
+++ src/front/pages/Easter.jsx
@@ -0,0 +1,31 @@
+import "../styles/easter.css";
+import "../styles/index.css";
+import { Grid } from "../components/Grid.jsx"
+
+
+export const EasterPage = () => {
+
+
+
+    return (
+        <>
+<div className="container-fluid easter">
+<Grid />
+</div>
+
+
+        {/* 
+
+<div className="row">
+
+    <div className="col">
+    <p className="congrats">You found the hidden lucky hans! Congratulations!</p>
+</div>
+
+<div className="col">
+    
+</div>
+
+</div>*/}
+        </>
+    );}
